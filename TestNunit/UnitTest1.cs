c170b0b9681e657a1f3d5a8101adbbf457e7fa15
Floran CHARBONNEL <FCHARB@CFCCI53.fr>
2017-04-18 10:53:40 +0200

    Test nunit 3

---
+++ TestNunit/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using JenkinsTP;
+
+namespace TestNunit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestAdd()
+        {
+            int result = Program.Add(10, 2);
+            NUnit.Framework.Assert.AreEqual(12, result);
+
+        }
+
+        [Test]
+        public void TestDiv()
+        {
+            int result = Program.Divider(10, 2);
+            NUnit.Framework.Assert.AreEqual(5, result);
+
+        }
+
+        [Test]
+        public void TestDiv0()
+        {
+            NUnit.Framework.Assert.Throws<DivideByZeroException>(() => JenkinsTP.Program.Divider(10, 0));
+
+        }
+
+    }
+}
