bcf376e5dfa72ec446d8d8ff17361a2de36f6ff0
Floran CHARBONNEL <FCHARB@CFCCI53.fr>
2017-04-18 14:19:45 +0200

    Test nunit 100

--- JenkinsTP/Program.cs
+++ JenkinsTP/Program.cs
@@ -20,12 +20,106 @@ namespace JenkinsTP
 
         public static void StringCopyCpy()
         {
-            Console.WriteLine("fdbfgdbiof bfhb çfdbiufhd bf hdubhfb fdb oifdb fdbhfd biofdhbiofh ibofdiobjfdpb j)ofdbi)ài kjb )f dikjbpfjbipofdj bipofdj bfjdbiofd jbfdjbf djbjfidpob pf fjiodp jbd j fd gf dg fdgdfgfdgf dg fdg d gfdgfdgfdgreyughoirejgoirejgiorehoiughrjgoiurheg rgo rug regoeà");
+            String couleur = null;
+
+            if(2 == 2)
+            {
+                 couleur = "rouge";
+            }
+
+            switch (couleur)
+
+            {
+
+                case "rouge":
+
+                    Console.WriteLine("Tu aimes la couleur du sang");
+
+                    break;
+
+
+                case "vert":
+
+                    Console.WriteLine("Tu aimes la nature");
+
+                    break;
+
+
+                case "bleu":
+
+                    Console.WriteLine("Tu aimes la couleur du ciel");
+
+                    break;
+
+
+                case "blanc":
+
+                    Console.WriteLine("Tu aimes la purete");
+
+                    break;
+
+
+                default:
+
+                    Console.WriteLine("Je n'ai aucune commentaire a faire");
+
+                    break;
+
+            }
+
+
         }
 
         public static void StringCopyCpy2()
         {
-            Console.WriteLine("fdbfgdbiof bfhb çfdbiufhd bf hdubhfb fdb oifdb fdbhfd biofdhbiofh ibofdiobjfdpb j)ofdbi)ài kjb )f dikjbpfjbipofdj bipofdj bfjdbiofd jbfdjbf djbjfidpob pf fjiodp jbd j fd gf dg fdgdfgfdgf dg fdg d gfdgfdgfdgreyughoirejgoirejgiorehoiughrjgoiurheg rgo rug regoeà");
+            String couleur = null;
+
+            if (2 == 2)
+            {
+                couleur = "rouge";
+            }
+
+            switch (couleur)
+
+            {
+
+                case "rouge":
+
+                    Console.WriteLine("Tu aimes la couleur du sang");
+
+                    break;
+
+
+                case "vert":
+
+                    Console.WriteLine("Tu aimes la nature");
+
+                    break;
+
+
+                case "bleu":
+
+                    Console.WriteLine("Tu aimes la couleur du ciel");
+
+                    break;
+
+
+                case "blanc":
+
+                    Console.WriteLine("Tu aimes la purete");
+
+                    break;
+
+
+                default:
+
+                    Console.WriteLine("Je n'ai aucune commentaire a faire");
+
+                    break;
+
+            }
+
+
         }
 
         public static int Divider(int a, int b)
