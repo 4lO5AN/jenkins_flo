75acd5b3955aa64554570cd6d9ec505ad4233cd7
floran50 <floran50@hotmail.fr>
2017-04-19 09:25:10 +0200

    rgrgfd

--- JenkinsTP/Program.cs
+++ JenkinsTP/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 [assembly: CLSCompliant(true)]
 namespace JenkinsTP
+
 {
     public static class Program
     {
@@ -16,110 +17,6 @@ namespace JenkinsTP
         public static int Add(int a, int b)
         {
             return a + b;
-        }
-
-        public static void StringCopyCpy()
-        {
-            String couleur = null;
-
-            if(2 == 2)
-            {
-                 couleur = "rouge";
-            }
-
-            switch (couleur)
-
-            {
-
-                case "rouge":
-
-                    Console.WriteLine("Tu aimes la couleur du sang");
-
-                    break;
-
-
-                case "vert":
-
-                    Console.WriteLine("Tu aimes la nature");
-
-                    break;
-
-
-                case "bleu":
-
-                    Console.WriteLine("Tu aimes la couleur du ciel");
-
-                    break;
-
-
-                case "blanc":
-
-                    Console.WriteLine("Tu aimes la purete");
-
-                    break;
-
-
-                default:
-
-                    Console.WriteLine("Je n'ai aucune commentaire a faire");
-
-                    break;
-
-            }
-
-
-        }
-
-        public static void StringCopyCpy2()
-        {
-            String couleur = null;
-
-            if (2 == 2)
-            {
-                couleur = "rouge";
-            }
-
-            switch (couleur)
-
-            {
-
-                case "rouge":
-
-                    Console.WriteLine("Tu aimes la couleur du sang");
-
-                    break;
-
-
-                case "vert":
-
-                    Console.WriteLine("Tu aimes la nature");
-
-                    break;
-
-
-                case "bleu":
-
-                    Console.WriteLine("Tu aimes la couleur du ciel");
-
-                    break;
-
-
-                case "blanc":
-
-                    Console.WriteLine("Tu aimes la purete");
-
-                    break;
-
-
-                default:
-
-                    Console.WriteLine("Je n'ai aucune commentaire a faire");
-
-                    break;
-
-            }
-
-
         }
 
         public static int Divider(int a, int b)
